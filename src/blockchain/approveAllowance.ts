6fdf3c0c7a7763e826b071904879ba473eddc010
idcetim <javirofe96@gmail.com>
2024-05-30 07:52:53 +0200

    Add approve and allowance token

--- src/blockchain/approveAllowance.ts
+++ src/blockchain/approveAllowance.ts
@@ -1,4 +1,16 @@
+import Web3 from "web3";
+import { EIP1193Provider } from "../types/Metamask";
+import { ABIS } from "./abis";
 
-export const approveToken = async () => {
+export const approveToken = async (provider: EIP1193Provider, userAddress: string, spender: string,  amountWei: string, tokenAddress: string) => {
+const web3 = new Web3(provider)
+const tokenContract = new web3.eth.Contract(ABIS.token, tokenAddress)
+await tokenContract.methods.approve(spender, amountWei).send({ from: userAddress })
+}
 
+export const getAllowance = async (provider: EIP1193Provider, userAddress: string, spender: string, tokenAddress: string): Promise<number> => {
+const web3 = new Web3(provider)
+const tokenContract = new web3.eth.Contract(ABIS.token, tokenAddress)
+const allowance: number = await tokenContract.methods.allowance(userAddress, spender).call()
+return allowance
 }
