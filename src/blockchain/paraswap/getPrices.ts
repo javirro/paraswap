84bd57d82d0134b5778e2457b9309800602da3bd
idcetim <javirofe96@gmail.com>
2024-06-01 07:09:37 +0200

    Add InputForm component

--- src/blockchain/paraswap/getPrices.ts
+++ src/blockchain/paraswap/getPrices.ts
@@ -1,18 +1,12 @@
 export interface GetParaswapPricesParams {
-  fromAddress: string;
-  destAddress: string;
-  amountWei: string;
-  fromDecimals: string;
-  destDecimals: string;
+  fromAddress: string
+  destAddress: string
+  amountWei: string
+  fromDecimals: string
+  destDecimals: string
 }
-export const getParaswapPrices = async ({
-  fromAddress,
-  destAddress,
-  amountWei,
-  fromDecimals,
-  destDecimals,
-}: GetParaswapPricesParams) => {
-  const baseURL = "https://apiv5.paraswap.io/prices/";
+export const getParaswapPrices = async ({ fromAddress, destAddress, amountWei, fromDecimals, destDecimals }: GetParaswapPricesParams) => {
+  const baseURL = "https://apiv5.paraswap.io/prices/"
   const params = {
     srcToken: fromAddress,
     destToken: destAddress,
@@ -21,15 +15,15 @@ export const getParaswapPrices = async ({
     destDecimals: destDecimals,
     side: "SELL",
     network: "56",
-  };
-  const url = new URL(baseURL);
-  Object.keys(params).forEach((key) => {
-    const param = params[key as keyof typeof params];
-    url.searchParams.append(key, param);
-  });
+  }
+  const url = new URL(baseURL)
+  Object.keys(params).forEach(key => {
+    const param = params[key as keyof typeof params]
+    url.searchParams.append(key, param)
+  })
 
   const response = await fetch(url.toString())
   const data = await response.json()
   const priceRoute = data.priceRoute
   return priceRoute
-};
+}
--- src/components/Swap/Swap.tsx
+++ src/components/Swap/Swap.tsx
@@ -1,10 +1,40 @@
-import './Swap.css'
-const Swap = () => {
-return(
-  <section id="swap">
+import { useEffect, useState } from "react"
+import { Balance } from "../../types/blockchain"
+import InputForm from "../InputForm/InputForm"
+import { EIP1193Provider } from "../../types/Metamask"
+import { getBalance } from "../../blockchain/walletFunctions/getBalance"
+import "./Swap.css"
 
-  </section>
-)
+interface SwapProps {
+  provider: EIP1193Provider
+  userAccount: string
+  chainId: string
+}
+const Swap = ({ provider, userAccount, chainId }: SwapProps) => {
+  const [balance, setBalance] = useState<Balance>({ weiBalance: "", ethBalance: "" })
+  const [amount, setAmount] = useState<string>("")
+  const [from, setFrom] = useState<string>("bnb")
+  const [to, setTo] = useState<string>("usdt")
+
+  const handleMaxAmount = () => {
+    setAmount(balance?.ethBalance)
+  }
+
+  useEffect(() => {
+    if (!provider) return
+    getBalance(provider, from, userAccount, chainId)
+      .then((b: Balance) => setBalance(b))
+      .catch(e => {
+        console.error("Error getting balance", e)
+      })
+  }, [provider, from, userAccount, chainId])
+
+  
+  return (
+    <section id="swap">
+      <InputForm amount={amount} setAmount={setAmount} userAccount={userAccount} from={from} setFrom={setFrom} handleMaxAmount={handleMaxAmount} />
+    </section>
+  )
 }
 
 export default Swap
