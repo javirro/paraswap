e04833638c8cf3a6976cc8f584416c19198e8077
idcetim <javirofe96@gmail.com>
2024-06-08 11:40:25 +0200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { EIP1193Provider } from "./types/Metamask"
 import { hexToDecimal } from "./utils/numberConversion"
 import DiscoverWalletProviders from "./components/DetectWallets/DetectWallets"
 import Swap from "./components/Swap/Swap"
+import Navbar from "./components/Navbar/Navbar"
 
 import "./App.css"
-
 
 function App() {
   const [provider, setProvider] = useState<EIP1193Provider>()
   const [userAccount, setUserAccount] = useState<string>("")
   const [chainId, setChainId] = useState<string>("56")
-  const [openWalletModal, setOpenWalletModal] = useState<boolean>(true)
+  const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
   provider?.on?.("accountsChanged", (accounts: any) => {
     setUserAccount(accounts[0])
   })
@@ -32,6 +32,7 @@ function App() {
           chaindId={chainId}
         />
       )}
+      <Navbar setOpenWalletModal={setOpenWalletModal} userAccount={userAccount} chainId={chainId} />
       {provider && <Swap provider={provider} userAccount={userAccount} chainId={chainId} />}
     </div>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { formatAddress } from "../../utils/formatAddress"
+import "./Navbar.css"
+
+interface NavbarProps {
+  setOpenWalletModal: (open: boolean) => void
+  userAccount: string
+  chainId: string
+}
+
+const Navbar = ({ setOpenWalletModal, userAccount, chainId }: NavbarProps) => {
+  const isConnected: boolean = userAccount !== ""
+  return (
+    <nav>
+      <div className="content">
+        <strong>Network: {chainId}</strong>
+        {!isConnected ? (
+          <button className="connect" onClick={() => setOpenWalletModal(true)}>
+            Connect Wallet{" "}
+          </button>
+        ) : (
+          <span> {formatAddress(userAccount)}</span>
+        )}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
